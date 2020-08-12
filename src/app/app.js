35eee43e38efee2322cff9fb2c3a45a69649c0ec
Przemysław Szelenberger <p.szelenberger@gmail.com>
2020-08-12 20:47:07 +0200

    Customise app

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.scss';
+
+export function App() {
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h1>
+          Button Component
+        </h1>
+      </header>
+    </div>
+  );
+}
---
+++ src/app/app.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './app';
+
+test('renders button component', () => {
+  const { getByText } = render(<App />);
+  const buttonComponent = getByText(/button component/i);
+
+  expect(buttonComponent).toBeInTheDocument();
+});
